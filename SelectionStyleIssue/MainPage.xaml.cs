f11401464034bcfede92f4adb3cef7b7ec17c5e6
Scharien <Todd.Scharien@gov.bc.ca>
2023-11-21 08:29:47 -0800

    Initial commit

---
+++ SelectionStyleIssue/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace SelectionStyleIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> Items { get; set; } =
+        [
+            "First",
+            "Second",
+            "Third",
+        ];
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CollectionView.ItemsSource = Items;    
+        }
+
+        private void CollectionView_Loaded(object sender, EventArgs e)
+        {
+            CollectionView.SelectedItem = Items[1];
+        }
+    }
+}
